87de6afb38b1b5f9713a5f189482f0a7c5330af0
Ray H <57116518+rayhellesoe@users.noreply.github.com>
2020-04-08 13:22:30 +1200

    Add files via upload
    update tutorial page

--- js/videosign.js
+++ js/videosign.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('#tooltip-message').on('show.bs.modal', function (e) {
     })
 
+    // Tooltips for Meeting Controls
     $(".meeting-button").mouseenter((e) => {
         const myEl = e.currentTarget
         const title = myEl.dataset.toolTitle
@@ -14,5 +15,35 @@ $(document).ready(function () {
 
     $(".meeting-button").mouseleave((e) => {
         $("#tooltip-message").modal('hide')
+    })
+
+    // Tooltips for Document Controls
+    $(".doc-tooltip").mouseenter((e) => {
+        const myEl = e.currentTarget
+        const title = myEl.dataset.toolTitle
+        const message = myEl.dataset.toolMessage
+
+        $('.modal-title').html(title)
+        $('.modal-body').html(message)
+        $("#tooltip-message").modal('show')
+    })
+
+    $(".doc-tooltip").mouseleave((e) => {
+        $("#tooltip-message").modal('hide')
+    })
+
+    // Controls for opening and closing document
+    $("#open-document").click(() => {
+        $(".logo-image, .menu-heading, .left-links, .center-panel").addClass("hide-element")
+        $(".meeting-button").css("text-align", "right")
+        $(".doc-view, .tool-menu").removeClass("hide-element")
+        $(".meeting-grid").addClass("doc-view-enabled")
+    })
+
+    $("#close-document").click(() => {
+        $(".logo-image, .menu-heading, .left-links, .center-panel").removeClass("hide-element")
+        $(".meeting-button").css("text-align", "left")
+        $(".doc-view, .tool-menu").addClass("hide-element")
+        $(".meeting-grid").removeClass("doc-view-enabled")
     })
 })
